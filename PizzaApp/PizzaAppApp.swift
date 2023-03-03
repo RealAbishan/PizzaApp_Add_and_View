f1f63d0d9dbf14d6299df2ce7d4d212c637b6a6f
Abishan <highoncode09@gmail.com>
2023-03-03 12:08:24 +0530

    Initial Commit

---
+++ PizzaApp/PizzaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAppApp.swift
+//  PizzaApp
+//
+//  Created by Abishan Parameswaran on 2023-03-03.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
