046e26da5b2a9b121ea2692ea5debc924077998b
Abishan <highoncode09@gmail.com>
2023-03-06 22:50:46 +0530

    Constant Cooler Added

---
+++ PizzaApp/Constants/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  PizzaApp
+//
+//  Created by Abishan Parameswaran on 2023-03-06.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants{
+    //static let oldPrimaryColor = Color()
+    static let newPrimaryColor = (Color(hue: 0.077, saturation: 1.0, brightness: 1.0))
+}
+
+
+//extension Color {
+//    static let oldPrimaryColor = Color(UIColor.systemIndigo)
+//    static let newPrimaryColor = Color(“primaryColor”)
+//}
